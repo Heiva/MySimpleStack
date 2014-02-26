9f841491d5a350cc47c19ded77bd5f09edf75163
20904873 <20904873@203-06.mig.snap.fsi>
2014-02-26 11:38:06 +0100

    question test

---
+++ MySimpleStack/src/test/java/dcll/adav/MySimpleStack/SimpleStackImplTest.java
@@ -0,0 +1,22 @@
+package dcll.adav.MySimpleStack;
+
+import junit.framework.Assert;
+
+public class SimpleStackImplTest {
+	SimpleStack simpleStack =  new SimpleStackImpl();;
+	
+	public void setUp() {
+	}
+	
+	public void test() {
+		Assert.assertEquals(true, simpleStack.isEmpty());
+		simpleStack.push(new Item(new String("toto")));
+		Assert.assertEquals(false, simpleStack.isEmpty());
+		simpleStack.push(new Item(new Integer(9)));
+		Assert.assertEquals(2, simpleStack.getSize());
+		Assert.assertEquals(9, simpleStack.pop().getValue());
+		Assert.assertEquals(1, simpleStack.getSize());
+		Assert.assertEquals(9, simpleStack.peek().getValue());
+		Assert.assertEquals(2, simpleStack.getSize());
+	}
+}
